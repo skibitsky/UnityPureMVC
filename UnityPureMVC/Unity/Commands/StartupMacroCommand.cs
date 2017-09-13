08b343930d62d13d5a1629877b3efe0d3f666ad3
Gleb Skibitsky <gl.skib@gmail.com>
2017-09-13 23:33:07 +0300

    Started working on Unity specific components

---
+++ UnityPureMVC/Unity/Commands/StartupMacroCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityPureMVC.Interfaces;
+using UnityPureMVC.Patterns;
+
+namespace UnityPureMVC.Unity.Commands
+{
+    public class StartupMacroCommand : MacroCommand
+    {
+        public override void Execute(INotification notification)
+        {
+            base.Execute(notification);
+        }
+    }
+}
---
+++ UnityPureMVC/Unity/UnityFacade.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+// ReSharper disable ArrangeAccessorOwnerBody
+
+namespace UnityPureMVC.Unity
+{
+    public class UnityFacade : Patterns.Facade
+    {
+        // ReSharper disable once InconsistentNaming
+        public const string STARTUP = "UnityFacade.StartUp";
+
+        static UnityFacade() { Instance = new UnityFacade(); }
+
+        // Override 
+#pragma warning disable 108,114
+        public static UnityFacade GetInstance { get { return Instance as UnityFacade; } }
+#pragma warning restore 108,114
+
+        public virtual void Startup()
+        {
+            SendNotification(STARTUP);
+        }
+
+    }
+}
